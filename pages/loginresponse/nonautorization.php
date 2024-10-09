f1eb938f0354509f5b3e91aa92342953c41549fd
Butterfly <mystybutterfly@proton.me>
2024-10-09 08:07:40 +0300

    Update nonautorization.php

--- pages/loginresponse/nonautorization.php
+++ pages/loginresponse/nonautorization.php
@@ -1,1 +1,1 @@
-<h2 style="text-align: center;color:blue">Ви не увійшли в систему. <a href="../index.php#login-container">Вхід/реєстрація</a></h2>
+<h2 style="text-align: center;color:blue" class="navigation-container">Ви не увійшли в систему. <a href="../index.php#login-container">Вхід/реєстрація</a></h2>
