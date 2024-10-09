35336c8be291a3261b1e8998af9c3e561f6996a9
Butterfly <mystybutterfly@proton.me>
2024-10-09 08:05:59 +0300

    Update fewemail.php

--- pages/loginresponse/fewemail.php
+++ pages/loginresponse/fewemail.php
@@ -15,7 +15,7 @@ include("./lrheader.php");
 ?>
 
 <div style="padding: 20px"><h2 style="text-align: center;color:blue">Помилка</h2>
-<div>Сталася помилка. Зверніться до адміністратора. <a href="../navigation/about.php#connectUs">Підтримка</a></div></div>
+<div class="navigation-container">Сталася помилка. Зверніться до адміністратора. <a href="../navigation/about.php#connectUs">Підтримка</a></div></div>
 </div>
 <footer>
   <div>©️ Lorem.com 2024</div>
