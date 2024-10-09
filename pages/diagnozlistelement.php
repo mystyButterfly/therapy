e69376e46527d85c1afa25cd6866008fd657c512
Butterfly <mystybutterfly@proton.me>
2024-10-09 07:59:02 +0300

    Update diagnozlistelement.php

--- pages/diagnozlistelement.php
+++ pages/diagnozlistelement.php
@@ -3,3 +3,4 @@
   <div class="combine-div">Next Step</div>
 </a>
 <h2 style="padding: 30px">[Here was javaScript logic]</h2>
+<p class="js-logic">Please be aware that some content has been temporarily removed due to intellectual property considerations.</p>
