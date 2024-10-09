d345988821e3160d876882af37ed1af70436ad85
Butterfly <mystybutterfly@proton.me>
2024-10-09 08:01:30 +0300

    Update about.php

--- pages/navigation/about.php
+++ pages/navigation/about.php
@@ -12,7 +12,7 @@ include('./header.php')
 ?>
 
 <h2 style="text-align: center;color:blue">Про нас</h2>
-<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus distinctio libero cupiditate, ex magni eveniet reprehenderit ea quia, vitae ipsam minus quo, possimus illum laborum maxime hic est. Corrupti explicabo soluta quod voluptatem dolorem, quis temporibus ullam! Id, molestias at a doloribus voluptates temporibus voluptas, esse illum dolor dicta quisquam.</div>
+<div class="navigation-container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus distinctio libero cupiditate, ex magni eveniet reprehenderit ea quia, vitae ipsam minus quo, possimus illum laborum maxime hic est. Corrupti explicabo soluta quod voluptatem dolorem, quis temporibus ullam! Id, molestias at a doloribus voluptates temporibus voluptas, esse illum dolor dicta quisquam.</div>
 
   <div id='connectUs' style="padding: 70px;">
     <h3>Контакти:</h3> <br/>
